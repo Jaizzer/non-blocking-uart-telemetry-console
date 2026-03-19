1163c597fc5d125d9316a6c5afaac46d79e91173
Jaizzer <florenzjaizzer.calderon@gmail.com>
2026-03-19 13:41:50 +0800

    chore: Setup working directory

---
+++ main.c
@@ -0,0 +1,7 @@
+#include "stm32f411xe.h"
+#include <stdint.h>
+
+int main(void) {}
+
+/* Stub for -nostdlib compilation to satisfy the Reset_Handler in startup_stm32f411xe.s */
+void __libc_init_array(void) { /* Empty stub */ }
